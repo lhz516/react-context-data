392841ccfa4ec6b20c2f68d48ee7f828353e4525
Hanz Luo <lhz0516@gmail.com>
2018-09-04 01:33:33 -0700

    change callback to options

--- src/create-store.js
+++ src/create-store.js
@@ -8,7 +8,7 @@ export default function createStore(initialState = {}) {
     return currentState
   }
 
-  function setState(newState, callback) {
+  function setState(newState, options = {}) {
     const oldState = currentState
     if (typeof newState === 'object' && newState !== null) {
       currentState = { ...currentState, ...newState }
@@ -18,13 +18,14 @@ export default function createStore(initialState = {}) {
       throw new Error('Invalid argument of store.setState')
     }
 
+    const { reactive = true } = options
+    if (!reactive) {
+      return
+    }
+
     listenHandlers.forEach((func) => {
       func(oldState, currentState)
     })
-
-    if (typeof cb === 'function') {
-      callback()
-    }
   }
 
   function listen(handler) {
